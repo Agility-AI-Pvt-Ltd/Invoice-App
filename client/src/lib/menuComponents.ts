9c54cfda8109ff21bda05adf68f2c53bf7da2ddb
Abhijit-Jha <abhijeetjha913@gmail.com>
2025-08-07 17:35:19 +0530

    merge conflict

--- client/src/lib/menuComponents.ts
+++ client/src/lib/menuComponents.ts
@@ -6,6 +6,7 @@ export const menuComponentMap: Record<string, React.LazyExoticComponent<() => Re
     dashboard: lazy(() => import("@/pages/dashboard")),
     "my-customers": lazy(() => import("@/pages/my-customer")),
     'inventory' : lazy(() => import("@/pages/inventory")),
+    'expenses-purchases' : lazy(() => import("@/pages/purchase")),
     invoices: lazy(() => import("@/pages/invoices")),
     "sales-revenue": lazy(() => import("@/pages/sales-revenue")),
     "expenses-purchases": lazy(() => import("@/pages/expenses-purchases")),
